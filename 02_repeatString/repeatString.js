1c71d79405c8eacfb06448dba20d705fdde3a04a
Mak <mahmoudcode913@gmail.com>
2021-09-05 17:10:43 -0500

    completed Ex 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, num = Math.floor(Math.random() * 1000)) {
+    let finalString= "";
+    if (num < 0){
+        return "ERROR"
+    }else{
+        for (let i = 0; i < num; i++) {
+            finalString += str;
+        }
+    }
+    
+    return finalString;
+
 
 };
 
